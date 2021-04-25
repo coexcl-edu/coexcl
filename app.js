11d96bca91854dfd68bf421d2201c29050960220
coexcl-edu <79722330+coexcl-edu@users.noreply.github.com>
2021-04-26 00:23:55 +0530

    Update app.js
    Correct File Checkin

--- app.js
+++ app.js
@@ -1,56 +1,42 @@
-//const express = require('express')
-//const mongoose = require('mongoose')
-//const url = "mongodb://localhost:27017/coexcldb";
-/*const port= process.env.port || 8080;
-app.use(express.json());
-
-const url = "mongodb+srv://coexcl:coexcl@cluster0.9nvpj.mongodb.net/coexcldb";
-
-const app = express()
-
-mongoose.connect(url, {useNewUrlParser:true,useUnifiedTopology: true })
-const con = mongoose.connection
-
-/*con.on('open', () => {
-    console.log('connected...')
-})
-
-app.get('/welcome', (req,res) => {
-  
-        res.send('WELCOME to Node JS Application');
-        
-}); 
-
-
-app.get('/', (req, res) => {
- res.status(200).send('Hello, world!').end();
-});
-
-const userRouter = require('./routes/aliens')
-app.use('/users',userRouter) 
-
-app.listen(port, () => {
-    console.log('Server started')
-}) */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 'use strict';
 
 // [START gae_node_request_example]
-const express = require('express');
 
+
+const express = require('express')
+const mongoose = require('mongoose')
+//const url = "mongodb://localhost:27017/coexcldb";
 const app = express();
+app.use(express.json());
+const url = "mongodb+srv://coexcl:coexcl@cluster0.9nvpj.mongodb.net/coexcldb";
+
+
+mongoose.connect(url, {useNewUrlParser:true,useUnifiedTopology: true })
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected...')
+})
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world!').end();
 });
 
-// Start the server
+const userRouter = require('./routes/aliens')
+app.use('/users',userRouter)
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
-// [END gae_node_request_example]
+
 
 module.exports = app;
 
