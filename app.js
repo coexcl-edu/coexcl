8eaee75e1b491147b20d25b22dfffde2c2232b52
coexcl-edu <79722330+coexcl-edu@users.noreply.github.com>
2021-04-25 14:34:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 //const url = "mongodb://localhost:27017/coexcldb";
-const port= process.env.port || 8080;
+/*const port= process.env.port || 8080;
 app.use(express.json());
 
 const url = "mongodb+srv://coexcl:coexcl@cluster0.9nvpj.mongodb.net/coexcldb";
@@ -19,18 +19,39 @@ app.get('/welcome', (req,res) => {
   
         res.send('WELCOME to Node JS Application');
         
-}); */
+}); 
 
 
 app.get('/', (req, res) => {
  res.status(200).send('Hello, world!').end();
 });
 
-/*const userRouter = require('./routes/aliens')
-app.use('/users',userRouter) */
+const userRouter = require('./routes/aliens')
+app.use('/users',userRouter) 
 
 app.listen(port, () => {
     console.log('Server started')
-})
+}) */
+
+'use strict';
+
+// [START gae_node_request_example]
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
 
 module.exports = app;
+
+
