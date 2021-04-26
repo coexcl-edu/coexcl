513792642e7b3d1af2cb57b24b81eb9d1e40b0a4
coexcl-edu <79722330+coexcl-edu@users.noreply.github.com>
2021-04-27 02:05:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.get('/', (req, res) => {
 const userRouter = require('./routes/userRoute')
 app.use('/users',userRouter)
 
-const videoRouter = require('./routes/videoRoute')
-app.use('/videos',videoRouter)
+//const videoRouter = require('./routes/videoRoute')
+//app.use('/videos',videoRouter)
 
 
 const PORT = process.env.PORT || 8080;
